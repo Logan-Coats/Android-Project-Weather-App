10028b4f76d74212d4495e20b0121e17c75e9230
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2022-09-30 15:21:01 -0500

    Removed code from interface, will be changed

--- app/src/main/java/com/example/androidgroupprojectf22/WeatherApi.java
+++ app/src/main/java/com/example/androidgroupprojectf22/WeatherApi.java
@@ -1,13 +1,11 @@
 package com.example.androidgroupprojectf22;
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.Retrofit;
 import retrofit2.http.GET;
 public interface WeatherApi {
+    // 706bfbd73c816293cefa0b85aa4d8479 openweathermap api key do not steal!
+    //url: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
-    @GET("posts")
-    Call<List> getPosts();
 
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com/")
-            .build();
 }

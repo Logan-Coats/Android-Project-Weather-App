3e57210301cdefe6d4a579757fa6d43ca4f65d90
Logan Coats <logancoats222@gmail.com>
2022-10-03 15:50:42 -0500

    successful api call, can be expanded upon to get lat+long from the phone itself

--- app/src/main/java/com/example/androidgroupprojectf22/MainActivity.java
+++ app/src/main/java/com/example/androidgroupprojectf22/MainActivity.java
@@ -3,6 +3,22 @@ package com.example.androidgroupprojectf22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void testApi(View v){
+        callApi(); // should log to console
+    }
+    private final String appid = "0ffb075f7e03483db28200427220310";
+    private final String location = "40,-94";
+    private final String url = "https://api.weatherapi.com/v1/current.json?key="+appid+"&q="+location;
+
+
+    public void callApi(){
+
+        StringRequest req = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("TAG","response: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TAG" , error.toString().trim());
+            }
+        });
+        RequestQueue reqQueue = Volley.newRequestQueue(this);
+        reqQueue.add(req);
+        
     }
 }

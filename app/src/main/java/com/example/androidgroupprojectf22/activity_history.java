705cbe768ffe930365d741fabf981da3a964a0ec
Casey Wormington <54279844+MuffinsBeGreat@users.noreply.github.com>
2022-10-21 12:29:25 -0500

    Finished the default code in onCreate in activity_history to make it functional.

--- app/src/main/java/com/example/androidgroupprojectf22/activity_history.java
+++ app/src/main/java/com/example/androidgroupprojectf22/activity_history.java
@@ -1,8 +1,11 @@
 package com.example.androidgroupprojectf22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -10,6 +13,8 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class activity_history extends AppCompatActivity {
+
+    private String location = "64468";
 
     private class RecyclerViewOnGestureListener extends GestureDetector.SimpleOnGestureListener {
         @Override
@@ -22,7 +27,6 @@ public class activity_history extends AppCompatActivity {
                     int position = holder.getAdapterPosition();
                     // Single tap
                     Log.d("click", "clicked on item " + position);
-
                     historyServer.notifyDataSetChanged();
                     return true;
                 }
@@ -36,6 +40,27 @@ public class activity_history extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        RecyclerView historyRecycler = findViewById(R.id.historyRV);
+        historyRecycler.setAdapter(historyServer);
+
+        Intent history = getIntent();
+
+        LinearLayoutManager myManager = new LinearLayoutManager(this);
+        historyRecycler.setLayoutManager(myManager);
+
+        GestureDetectorCompat detector = new GestureDetectorCompat(this,
+                new RecyclerViewOnGestureListener());
+        historyRecycler.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                return detector.onTouchEvent(e);
+            }
+        });
+
+//        location = history.getStringExtra("Location");
+//        Log.d("loc", "Location: " + location);
+
+
     }
 
     private HistoryModel model = HistoryModel.getModel();
